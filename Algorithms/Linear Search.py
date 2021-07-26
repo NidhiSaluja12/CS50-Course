0fec35d1cbf95cda95dc3229c74ebfcb3cd33c33
Nidhi Saluja <84669505+NidhiSaluja12@users.noreply.github.com>
2021-07-26 11:42:17 +0530

    Update Linear Search.py

--- Algorithms/Linear Search.py
+++ Algorithms/Linear Search.py
@@ -1,1 +1,26 @@
+'''
+Pseudo Code:
+
+For i from 0 to size
+  If number is at ith position
+    return True
+    
+  Else
+    return False
+
+'''
+
+def LinearSearch(arr, n, s):
+    result = False
+    for i in range(n):
+        if arr[i] == s:
+            result = True
+    return result
+
+
+arr = list(map(int, input().split()))
+n = len(arr)
+s = int(input())
+res = LinearSearch(arr, n, s)
+print(res)
 
